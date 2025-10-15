e44f89d6739ce5ed735bffbac0c87ee9d35fc588
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-10-16 01:53:01 +0200

    Changed the name of the packages folder to app-packages to clearer indicate which file it's part of.

--- hosts/myhosts/app-packages.nix
+++ hosts/myhosts/app-packages.nix
@@ -1,18 +1,18 @@
 { config, pkgs, ... }:
 
 let
-  audio         = import ./packages/audio.nix { inherit pkgs; };
-  communication = import ./packages/communication.nix { inherit pkgs; };
-  development   = import ./packages/development.nix { inherit pkgs; };
-  drivers       = import ./packages/drivers.nix { inherit pkgs; };
-  editing       = import ./packages/editing.nix { inherit pkgs; };
-  gaming        = import ./packages/gaming.nix { inherit pkgs; };
-  security      = import ./packages/security.nix { inherit pkgs; };
-  utilities     = import ./packages/utilities.nix { inherit pkgs; };
-  video         = import ./packages/video.nix { inherit pkgs; };
+  audio         = import ./app-packages/audio.nix { inherit pkgs; };
+  communication = import ./app-packages/communication.nix { inherit pkgs; };
+  development   = import ./app-packages/development.nix { inherit pkgs; };
+  drivers       = import ./app-packages/drivers.nix { inherit pkgs; };
+  editing       = import ./app-packages/editing.nix { inherit pkgs; };
+  gaming        = import ./app-packages/gaming.nix { inherit pkgs; };
+  security      = import ./app-packages/security.nix { inherit pkgs; };
+  utilities     = import ./app-packages/utilities.nix { inherit pkgs; };
+  video         = import ./app-packages/video.nix { inherit pkgs; };
 in
 {
-  environment.systemPackages = with pkgs; concatLists [
+  environment.systemapp-packages = with pkgs; concatLists [
     audio
     communication
     development
