b9ebeac3cb53bd1fb296ab007adcca85b6688bf5
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-10-15 03:38:35 +0200

    Switched some Wine stuff over to the wine.nix file from audio.nix.

--- hosts/myhosts/app-configs/wine.nix
+++ hosts/myhosts/app-configs/wine.nix
@@ -8,6 +8,10 @@
         wine64Packages.stable
         wineWowPackages.stable  # 32-bit support
         winetricks               # handy helper scripts
+
+        # Wine audio support
+        faudio
+        vkd3d            # Vulkan Direct3D bridge (needed for some games/audio in Wine)
     ];
 
     ####################
--- hosts/myhosts/packages/audio.nix
+++ hosts/myhosts/packages/audio.nix
@@ -10,8 +10,4 @@
   alsaUtils        # ALSA utilities
   alsaLib          # ALSA library
   pulseaudio       # for compatibility if needed
-
-  # Wine audio support
-  faudio
-  vkd3d            # Vulkan Direct3D bridge (needed for some games/audio in Wine)
 ]
