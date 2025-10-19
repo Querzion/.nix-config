024f8a54cbcd49ada01df3e1ae4f3da4cffaf0cd
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-10-19 22:59:53 +0200

    Rewrote the install configuration in flake.nix.

--- flake.nix
+++ flake.nix
@@ -19,8 +19,8 @@
           home-manager.nixosModules.home-manager
           {
             # make home-manager CLI available system-wide
-            environment.systemPackages = with pkgs; [
-                home-manager
+            environment.systemPackages = [
+                pkgs.home-manager
             ];
           
             home-manager.useGlobalPkgs = true;
