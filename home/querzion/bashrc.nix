e753b836c71db9d5a1764859c088f99af9111871
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-10-21 05:38:56 +0200

    Added a . in front of nix-config in the bashrc commands.

--- home/querzion/bashrc.nix
+++ home/querzion/bashrc.nix
@@ -18,10 +18,10 @@
         # --------------------------
         # NixOS Flake management
         # --------------------------
-        alias nos-new='cd ~/nix-config && sudo nixos-rebuild switch --flake .#myhost'
-        alias nos-update='cd ~/nix-config && nix flake update --flake'
-        alias nos-upgrade='cd ~/nix-config && sudo nixos-rebuild switch --upgrade --flake .#myhost'
-        alias nos-check='cd ~/nix-config && nixos-rebuild build --flake .#myhost'
+        alias nos-new='cd ~/.nix-config && sudo nixos-rebuild switch --flake .#myhost'
+        alias nos-update='cd ~/.nix-config && nix flake update --flake'
+        alias nos-upgrade='cd ~/.nix-config && sudo nixos-rebuild switch --upgrade --flake .#myhost'
+        alias nos-check='cd ~/.nix-config && nixos-rebuild build --flake .#myhost'
 
         # --------------------------
         # Git shortcuts
