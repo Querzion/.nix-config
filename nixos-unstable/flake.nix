2c34ab11e57e3ced4643abbc1e4010756c6ad2ea
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-10-26 08:25:31 +0100

    M  nixos-unstable/flake.nix

--- nixos-unstable/flake.nix
+++ nixos-unstable/flake.nix
@@ -31,4 +31,48 @@
       };
     };
 }
+{
+  description = "Slisk Lindqvist NixOS + Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+
+      overlays = [
+        (self: super: {
+          # Disable onetbb tests
+          onetbb = super.onetbb.overrideAttrs (oldAttrs: {
+            doCheck = false;
+          });
+        })
+      ];
+    in
+    {
+      nixosConfigurations.myhost = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        nixpkgs.overlays = overlays;
+
+        modules = [
+          ./hosts/myhosts/configuration.nix
+          home-manager.nixosModules.home-manager
+          ({ config, pkgs, ... }: {
+            environment.systemPackages = [
+              pkgs.home-manager
+            ];
+
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.querzion = import ./home/querzion/home.nix;
+          })
+        ];
+      };
+    };
+}
 
