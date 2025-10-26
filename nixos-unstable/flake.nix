77be56f0decce66bfc1c13421292523f0ad4d45c
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-10-26 08:26:52 +0100

    M  nixos-unstable/flake.nix

--- nixos-unstable/flake.nix
+++ nixos-unstable/flake.nix
@@ -10,43 +10,9 @@
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
-    in
-    {
-      nixosConfigurations.myhost = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./hosts/myhosts/configuration.nix
-          home-manager.nixosModules.home-manager
-          # <-- module function with pkgs in scope
-          ({ config, pkgs, ... }: {
-            environment.systemPackages = [
-              pkgs.home-manager
-            ];
-
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.querzion = import ./home/querzion/home.nix;
-          })
-        ];
-      };
-    };
-}
-{
-  description = "Slisk Lindqvist NixOS + Home Manager configuration";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      system = "x86_64-linux";
 
       overlays = [
         (self: super: {
-          # Disable onetbb tests
           onetbb = super.onetbb.overrideAttrs (oldAttrs: {
             doCheck = false;
           });
@@ -56,7 +22,6 @@
     {
       nixosConfigurations.myhost = nixpkgs.lib.nixosSystem {
         inherit system;
-
         nixpkgs.overlays = overlays;
 
         modules = [
